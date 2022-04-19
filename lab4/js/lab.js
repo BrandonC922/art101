dfb7ed83e3c1d31ed38f4830954cf696a8992b67
BrandonC922 <102686166+BrandonC922@users.noreply.github.com>
2022-04-18 20:25:03 -0700

    fixed title

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -7,21 +7,21 @@
 
 // Pseudo Code for Everyday Task:
 //
-// Gather Supplies: a bowl, favorite cereal, milk, spoon.
-// Pour desired amount of cereal into bowl
-// Pour desired amount of milk into the same bowl
-// Put the spoon into the bowl.
-// Get cereal and milk on the spoon.
-// Put the contents of the spoon into your mouth.
-// Chew and swallow food
+// 1.Gather Supplies: a bowl, favorite cereal, milk, spoon.
+// 2.Pour desired amount of cereal into bowl
+// 3.Pour desired amount of milk into the same bowl
+// 4.Put the spoon into the bowl.
+// 5.Get cereal and milk on the spoon.
+// 6.Put the contents of the spoon into your mouth.
+// 7.Chew and swallow food
 // Repeat steps 4-7 until the bowl is empty.
 //
 // Pseudo Code Computer Task:
 //
-// Display numbers and symbols
-// Accept user input of equation
-// Validate user equation
-// Locate appropriate operations for given equation
-// Calculate result of user equation
-// Validate result of equation
-// Display result
+// 1.Display numbers and symbols
+// 2.Accept user input of equation
+// 3.Validate user equation
+// 4.Locate appropriate operations for given equation
+// 5.Calculate result of user equation
+// 6.Validate result of equation
+// 7.Display result
