4b03c16c29771df02f996c7e7efb0efcb11a2364
BrandonC922 <102686166+BrandonC922@users.noreply.github.com>
2022-04-18 20:07:25 -0700

    LAB4

---
+++ lab4/js/lab.js
@@ -0,0 +1,27 @@
+// Summary. This is PseudoCode
+// Description. PseudoCode to make a calculator and to make a bowl of cereal.
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author Brandon Christensen.
+// @since  2022
+
+// Pseudo Code for Everyday Task:
+//
+// Gather Supplies: a bowl, favorite cereal, milk, spoon.
+// Pour desired amount of cereal into bowl
+// Pour desired amount of milk into the same bowl
+// Put the spoon into the bowl.
+// Get cereal and milk on the spoon.
+// Put the contents of the spoon into your mouth.
+// Chew and swallow food
+// Repeat steps 4-7 until the bowl is empty.
+//
+// Pseudo Code Computer Task:
+//
+// Display numbers and symbols
+// Accept user input of equation
+// Validate user equation
+// Locate appropriate operations for given equation
+// Calculate result of user equation
+// Validate result of equation
+// Display result
