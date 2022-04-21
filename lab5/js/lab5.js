1b3cfee1c06d6575408cd098ba86137685b1da06
BrandonC922 <102686166+BrandonC922@users.noreply.github.com>
2022-04-20 18:02:43 -0700

    LAB_5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,24 @@
+/**
+ * Author:    Brandon Christensen
+ * Created:   April 19, 2022
+ **/
+
+//Define Varibales
+ var make = "Tesla";
+ var model = "S";
+ var color = "Black";
+ var year = 2012;
+
+ var ownIt = true;
+
+ // calculate
+var age = 2022 - year;
+
+// output
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Own It: " + ownIt + "<br>");
+document.writeln("Age: " + age + "<br>");
